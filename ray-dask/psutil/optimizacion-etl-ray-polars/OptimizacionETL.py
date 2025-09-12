8c43df3cda7bb565c27ad43bd52ff9ed96036a62
sm7ss <ns.7m.sw@gmail.com>
2025-09-12 05:28:34 -0100

    Update OptimizacionETL.py

--- ray-dask/psutil/optimizacion-etl-ray-polars/OptimizacionETL.py
+++ ray-dask/psutil/optimizacion-etl-ray-polars/OptimizacionETL.py
@@ -1,15 +1,6 @@
-#Importar las librerías importantes 
-import polars as pl 
+import psutil 
 import time
-import ray 
-import pyarrow as pa 
-import logging 
-import psutil
-import yaml
-from pydantic import BaseModel, field_validator, Field
-from pathlib import Path
 from dataclasses import dataclass
-from typing import List
-from enum import Enum
-#Wip 
 
+
+#Wip
