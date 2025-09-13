854e25d803908bfd31edd361e0cf706c0eab90b0
sm7ss <ns.7m.sw@gmail.com>
2025-09-13 05:29:29 -0100

    Update ResourceETL.py
    Eh bueno, se adapataron algunas cosas como las clases y la separacion, una es para el inicio en ray y la otra es para monitorear recursos basicos. Ejecutar esto (en caso de ejecutarlo con MultiStage.py unicamente llamar el pipeline de ray sin los parentesis para no sacar error puesto que el pipeline no retorna nada, en caso de usarlo para otra funcion entonces poner los parentesis siempre y cuando se retorne algo...

--- ray-dask/psutil/optimizacion-etl-ray-polars/ResourceETL.py
+++ ray-dask/psutil/optimizacion-etl-ray-polars/ResourceETL.py
@@ -1,43 +1,37 @@
 import psutil 
 import time
-from dataclasses import dataclass
 
-@dataclass
-class Recursos: 
-    cpu_logica: int
-    cpu_fisica: int
-    ram_disponible_GB: float
-    uso_cpu: float 
-    
-    @classmethod
-    def ray_info_available(cls): 
-        return cls(
-            ram_disponible=psutil.virtual_memory().available / (1024**3), 
-            tamaño_de_memoria=min()
-        )
-
-class Rescursos: 
-    def __init__(self):
-        self.cpu_logica = psutil.cpu_count(logical=True)
-        self.cpu_fisica = psutil.cpu_count(logical=False)
-        self.ram_disponible = psutil.virtual_memory().available / (1024**3)
-        self.uso_cpu = psutil.cpu_percent(interval=0.5)
-    
-    @classmethod
-    def ray_info_disponible(self, cls): 
-        return cls(
-            object_store_memory=min(self.ram_disponible*0.3, self.ram_disponible*(1024**3)*1.5), 
-            
-            
-        )
-
-class MonitorearRecursos: 
+class ResorceConfig: 
     @staticmethod
-    def resource(): 
-        pass
-    
-    @staticmethod
-    def monitoreo_recursos(): 
-        pass
+    def init_ray(tamaño_archivo: int): 
+        ram_total = psutil.virtual_memory().available
+        
+        cpu_fisico= max(1, psutil.cpu_count(logical=False)-1)
+        
+        object_store_memory = max(ram_total*0.3, tamaño_archivo*1.5)
+        
+        return {
+            "num_cpus": cpu_fisico, 
+            'object_store_memory': object_store_memory 
+        }
 
-#Wip
+class ResourceMonitoring: 
+    @staticmethod
+    def monitoreo(funcion, *args, **kwargs): 
+        tiempo_inicio = time.time()
+        memoria_inicio = psutil.virtual_memory().used
+        cpu_inicio = psutil.cpu_percent()
+        
+        resultado = funcion(*args, **kwargs)
+        
+        tiempo_final = time.time()
+        memoria_final = psutil.virtual_memory().used
+        cpu_final = psutil.cpu_percent()
+        
+        return {
+            'resultado':resultado, 
+            'timepo_segundos':tiempo_final-tiempo_inicio, 
+            'memoria_GB': (memoria_final-memoria_inicio) / (1024**3), 
+            'cpu_porcentaje_prom': (cpu_inicio+cpu_final)/2
+        }
+
