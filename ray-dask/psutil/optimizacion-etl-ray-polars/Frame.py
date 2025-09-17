0d7561d79767c74426afde60d4175a4dfd9b0df5
sm7ss <ns.7m.sw@gmail.com>
2025-09-17 03:20:01 -0100

    Update Frame.py
    Se adapto para poder leer archivos con un formato eager o lazy (para polars) y ray (para distribucion). La idea en sí es, que en base al pipeline general se obtenga un sistema que se autoajuste a los recursos y tamaño del archivo, ejemplo: si el archivo cabe en memoria usar eager, si no lazy, ¿demasiados datos? ray

--- ray-dask/psutil/optimizacion-etl-ray-polars/Frame.py
+++ ray-dask/psutil/optimizacion-etl-ray-polars/Frame.py
@@ -1,14 +1,28 @@
-#Importamos librerías 
-import ray 
-from ReadData import ReadFile
+#Importacion de librerias importantes 
+import ray
+import polars as pl 
+from pathlib import Path
 
-class GetFrame: 
-    def __init__(self, archivo: str):
-        model = ReadFile(archivo=archivo).read_file()
-        self.model_file = model.input_path
+class EagerFramStrategy: 
+    @staticmethod
+    def leer(path: Path) -> pl.DataFrame: 
+        if path.suffix == '.csv': 
+            return pl.read_csv(path)
+        else:
+            return pl.read_parquet(path)
     
-    def ray_frame(self) -> ray.data.Dataset: 
-        if self.model_file.suffix == '.csv': 
-            return ray.data.read_csv(self.model_file)
+    @staticmethod
+    def leer(path: Path) -> pl.LazyFrame: 
+        if path.suffix == '.csv': 
+            return pl.scan_csv(path) 
         else: 
-            return ray.data.read_parquet(self.model_file)
+            return pl.scan_parquet(path)
+
+class RayFrame: 
+    @staticmethod
+    def leer(path: Path) -> ray.data.Dataset: 
+        if path.suffix == '.csv': 
+            return ray.data.read_csv(path)
+        else: 
+            return ray.data.read_parquet(path)
+
